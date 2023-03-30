b64565479db9fc18a00cb9a479506fb69348eaaf
ValeraNazarchuk <valera55555nazarchuk@gmail.com>
2023-03-30 12:37:51 +0300

    add comments for js code

--- frontend/app/js/authentication.js
+++ frontend/app/js/authentication.js
@@ -1,41 +1,57 @@
 'use strict'
 
+// ___Подія для першої сторінки і сторінки забув пароль__
+
+// переміні форми authentication__login
 const LoginForm = document.querySelector('#authentication__login')
 const forgotBtn = document.querySelector('#authentication__login-forgot')
+const inputUsername = document.querySelector('#authentication-input__username')
+const inputPassword = document.querySelector('#authentication-input__password')
+const loginBtn = document.querySelector('#authentication__login-button')
+
+// переміні форми authentication__password
 const passwordForm = document.querySelector('#authentication__password')
 const backBtn = document.querySelector('#authentication__password-back')
 
+// Подія для кнопки *Back to Log In*
 forgotBtn.addEventListener('click', () => {
   LoginForm.style.display = 'none'
   passwordForm.style.display = 'flex'
 })
 
+// Подія для кнопки *Forgot password?*
 backBtn.addEventListener('click', () => {
   passwordForm.style.display = 'none'
   LoginForm.style.display = 'flex'
 })
 
-const inputUsername = document.querySelector('#authentication-input__username')
-const inputPassword = document.querySelector('#authentication-input__password')
-const loginBtn = document.querySelector('#authentication__login-button')
-const emailAddressBlock = document.querySelector(
-  '#authentication__email-address'
-)
-const emailCodeBlock = document.querySelector('#authentication__email-code')
-const addressInput = document.querySelector('#authentication__address-input')
-const addressBtn = document.querySelector('#authentication__address-btn')
-const codeInput = document.querySelector('#authentication__code-input')
-
+// Подія для кнопки *Log in*
 loginBtn.addEventListener('click', (e) => {
+  // Виключив перезагрузку сторінки при кліку
   e.preventDefault()
 
+  // провірка чи є якийсь текст в інпутах при нажаті на кнопку
   if (inputUsername.value && inputPassword.value) {
     LoginForm.style.display = 'none'
     emailAddressBlock.style.display = 'flex'
   }
 })
 
+// _______перемені форми authentication__email____
+
+// переміні блоку authentication__email-address
+const emailAddressBlock = document.querySelector('#authentication__email-address')
+const addressInput = document.querySelector('#authentication__address-input')
+const addressBtn = document.querySelector('#authentication__address-btn')
+
+// переміні блоку authentication__email-code
+const emailCodeBlock = document.querySelector('#authentication__email-code')
+const codeInput = document.querySelector('#authentication__code-input')
+
+// Подія для кнопки *Send Code*
 addressBtn.addEventListener('click', () => {
+
+  // перевірка чи є якийсь текст в інпуті при нажаті на кнопку
   if (addressInput.value) {
     emailAddressBlock.style.display = 'none'
     emailCodeBlock.style.display = 'flex'
