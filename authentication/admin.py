bc6e24625f19affadb56a83dc1580ea928de8b0f
ngwr21 <igoronik11@gmail.com>
2023-04-05 14:00:45 +0300

    some few fixes

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from .models import *
-
-
-@admin.register(UserProfile)
-class ProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'avatar')
-    search_fields = ('user', 'avatar')
-    list_filter = ('user', 'avatar')
-    ordering = ('user', 'avatar')
-
+#
+#
+# @admin.register(UserProfile)
+# class ProfileAdmin(admin.ModelAdmin):
+#     list_display = ('user', 'avatar')
+#     search_fields = ('user', 'avatar')
+#     list_filter = ('user', 'avatar')
+#     ordering = ('user', 'avatar')
+#
 
